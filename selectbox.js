d198fab5b9141010bd27dcdfe34af1f77ef27655
Joziel <joziel99@gmail.com>
2014-03-14 13:54:48 -0300

    Update selectbox.js

--- selectbox.js
+++ selectbox.js
@@ -1,9 +1,10 @@
-/*<div class="selectbox" data-id="multiselect" data-type="hover">
-	<input type="hidden" name="" value="">
-	<span class="title">Title</span>
-	<ul>
-		<li data-value="value" class="select">Label</li>
-		<li data-value="value" class="">Label</li>
+/*<div class="selectbox" data-id="multiselect">
+	<div class="box">
+		<input type="hidden" name="" value="">
+		<span class="title"></span>
+	</div>
+	<ul class="down">	
+		<li data-value=""></li>
 	</ul>
 </div>*/
 jQuery.fn.selectBox = function (){
@@ -14,21 +15,32 @@ jQuery.fn.selectBox = function (){
 	this.each(function(){
 		dataId = $(this).attr("data-id");
 		dataType = $(this).attr("data-type");
-
-		if(dataId == "multiselect"){
-			if ($(this).find('input[type="hidden"]').val()) {
+		if(dataId == "multiselect"){	
+			if ($(this).find('input[type="hidden"]').val() && $(this).find('input[type="hidden"]').val() != "-1") {
 				var values = $(this).find('input[type="hidden"]').val().split(',');
-				$(this).find('.title').html(values.join(","));
+
+				var removeItem = "-1";
+				newArray = new Array();
+				 jQuery.each(values,function(e, a){
+					if(removeItem != a){
+		        		newArray.push(a);
+		        	}
+		    	});
+				values = newArray;
+				$(this).find('input[type="hidden"]').val(values.join(","))
+				$(this).find('.title').html(values.length + " Selecionados");
+
 			}else{
 				var values = new Array();
+				$(this).find('input[type="hidden"]').val("-1");
 			}
 
 			jQuery.each(values, function(e , a){
-				$(this).find("ul li[data-value='"+a+"']").addClass("selected");
+				$($(this).find('ul li[data-value="'+a+'"]').selector).addClass("selected");
 			});
 			
 			$(this).find(".box").live("click",function(e){
-				e.stopImmediatePropagation()
+				e.stopImmediatePropagation();
 				if($(this).parent().find("ul").hasClass("open")){
 					$(this).parent().find("ul").removeClass("open");
 				}else{
@@ -38,20 +50,36 @@ jQuery.fn.selectBox = function (){
 				
 			});
 
-			$(this).find("ul li").live("click",function(){
-				if($(this).hasClass("selected")){
-					$(this).removeClass("selected");
-					var removeItem = $(this).attr("data-value");
-					newArray = new Array();
-					 jQuery.each(values,function(e, a){
-						if(removeItem != a){
-			        		newArray.push(a);
-			        	}
-			    	});
-					values = newArray;
+			$(this).find("ul li").live("click",function(e){
+				e.stopImmediatePropagation();
+				if($(this).attr("data-value") != "-1"){
+					if($(this).hasClass("selected")){
+						$(this).removeClass("selected");
+						values = $(this).closest(all.selector).find('input[type="hidden"]').val().split(',');
+						var removeItem = $(this).attr("data-value");
+						newArray = new Array();
+						 jQuery.each(values,function(e, a){
+							if(removeItem != a){
+				        		newArray.push(a);
+				        	}
+				    	});
+						values = newArray;
+						$(this).parent().find('li[data-value="-1"]').removeClass('selected');
+					}else{
+						if(values = $(this).closest(all.selector).find('input[type="hidden"]').val() != "-1"){
+							values = $(this).closest(all.selector).find('input[type="hidden"]').val().split(',');
+						}else{
+							values = new Array;
+						}
+						$(this).addClass("selected");
+						values.push($(this).attr("data-value"));
+						$(this).parent().find('li[data-value="-1"]').removeClass('selected');
+					}
 				}else{
+					$(this).parent().find('li').removeClass('selected');
 					$(this).addClass("selected");
-					values.push($(this).attr("data-value"));	
+					values = new Array;
+					values[0] = "-1";
 				}
 
 				if(values.length != 0){
@@ -63,19 +91,27 @@ jQuery.fn.selectBox = function (){
 					}
 					
 				}else{
-					$(this).parent().parent().find('.title').html("Indiferente");
-					$(this).parent().parent().find('input[type="hidden"]').val("");
+					$(this).parent().parent().find('.title').html("Selecione");
+					$(this).parent().parent().find('input[type="hidden"]').val("-1");
 				}
 				
 
 			});
+
+			
+
 		}else{
-			if ($(this).find('input[type="hidden"]').val()) {
+			if ($(this).find('input[type="hidden"]').val() && $(this).find('input[type="hidden"]').val() != "-1") {
 				var value = $(this).find('input[type="hidden"]').val();
+				$(this).find(".box .title").html($(this).find("ul li[data-value='"+value+"']").html());
 			}else{
 				var value = "";
+				$(this).find('input[type="hidden"]').val("-1");
 			}
 			
+
+			$(this).find('ul li[data-value="'+value+'"]').addClass("selected");
+
 			$(this).find(".box").live("click",function(e){
 				e.stopImmediatePropagation()
 				if($(this).parent().find("ul").hasClass("open")){
@@ -104,8 +140,8 @@ jQuery.fn.selectBox = function (){
 					$(this).parent().parent().find('input[type="hidden"]').val(value);
 					$(this).parent().parent().find('.title').html(label);
 				}else{
-					$(this).parent().parent().find('.title').html("Indiferente");
-					$(this).parent().parent().find('input[type="hidden"]').val("");
+					$(this).parent().parent().find('.title').html("Selecione");
+					$(this).parent().parent().find('input[type="hidden"]').val("-1");
 				}
 				
 
